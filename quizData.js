e1187586e759934fbd23b9a6979d85eabe2c7a35
EloinaCarrillo <elodinda04@gmail.com>
2019-11-03 08:54:34 -0600

    Create quizData.js

---
+++ quizData.js
@@ -0,0 +1,36 @@
+export const quizData = [
+  {
+    id: 0,
+    question: ` Hace referencia a los procesos, las funciones y los métodos que brinda una determinada biblioteca de programación ?`,
+    options: [`API`, `HTML`, `AWS`, `Swagger`],
+    answer: `API`
+  },
+  {
+    id: 1,
+    question: `Es un estilo de arquitectura de software que separa los datos de una aplicación, la interfaz de usuario, y la lógica de control en tres componentes distintos`,
+    options: [`CRUD`, `SCRUM`, `MVC`, `XP`],
+    answer: `MVC`
+  },
+  
+  {
+    id: 3,
+    question: `Son un mecanismo que nos permite establecer un enlace entre dos programas que se ejecutan independientes el uno del otro ?`,
+    options: [`Sockets`, `Aplicaciones moviles`, `Vistas`, `Interfaz`],
+    answer: `Sockets`
+  },
+  {
+    id: 4,
+    question: `Es un modelo de programación que consiste en la transformación de las tablas de una base de datos, en una serie de entidades que simplifiquen las tareas básicas de acceso a los datos para el programador?`,
+    options: [`Logg`, `ORM`, `HTTP`, `NPM`],
+    answer: `ORM`
+
+  },
+  {
+    id: 5,
+    question: `Es un método para trabajar en equipo a partir de iteraciones o Sprints.?`,
+    options: [`Scrum`, `Gmail`, `Zuagger`, `Azure`],
+    answer: `Scrum`
+  },
+];
+
+
